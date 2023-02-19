23cd80483dd53d0e60f2aeaad55454466d5c1ccd
coazz <aco94@hotmail.com>
2023-02-19 12:45:40 +0100

    first commit

---
+++ src/pages/Store.tsx
@@ -0,0 +1,18 @@
+import { storeItems } from "../data/items.js"
+import { Row, Col } from "react-bootstrap"
+import { Storeitem, StoreItemProps } from "../components/Storeitem.js"
+
+export function Store() {
+    return (
+        <>
+            <h1>Store</h1>
+            <Row md={2} xs={1} lg={3} className="g-3">
+                {storeItems.map((item: StoreItemProps) => (
+                    <Col key={item.id}>
+                        <Storeitem {...item} />
+                    </Col>
+                ))}
+            </Row>
+        </>
+    )
+}
